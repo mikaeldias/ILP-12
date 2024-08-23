e76966d42494f1ef8ed1e5343decafaa25510cc2
mikaeldias <mikaelsd1919@icloud.com>
2024-08-23 16:06:41 -0300

    Resolução questão 06 lista 4 arrays

--- atividade_4_arrays_ILP/06.py
+++ atividade_4_arrays_ILP/06.py
@@ -1,4 +1,15 @@
-contador = 0
+# resolução questão 6
+print('----------Seja Bem Vindo-----------')
 lista_numeros = []
-while ...:
-    numeros = int(input('Digite um valor para armazenar na lista: '))
+lista_string = []
+
+qtd_valores_listas = int(input('\nPor favor, digite a quantidade de itens que deseja adcionar nas listas de numeros e string(Ambas vão conter a mesma quantidade de caracteres): '))
+for x in range(qtd_valores_listas):
+    numeros = int(input('Digite um número inteiro para adcionar a lista: '))
+    string = input('Digite uma letrar para adcionar na lista de strings: ')
+    lista_numeros += [numeros]
+    lista_string += [string]
+    if x % 2 != 0:
+        lista_numeros[x] = lista_string[x]
+
+print(lista_numeros)
