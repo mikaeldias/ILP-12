64278b3023fec3215b2dae15db1c8f32b280773f
mikaeldias <mikaelsd1919@icloud.com>
2024-08-25 14:23:39 -0300

    Resolução questão 8

--- atividade_4_arrays_ILP/08.py
+++ atividade_4_arrays_ILP/08.py
@@ -1,14 +1,20 @@
-entrada = input('Digite valores inteiros, separados por espaço: ')
-#list comprehension
-lista_string_inteiros = [int(num) for num in entrada.split()] 
-so_impar = []
-texte = []
-for i in lista_string_inteiros:
-    if i % 2 != 0:
-        so_impar.append(i)
-    else:
-        pass
+inteiros = [] # lista que vai armazenar os valores inputados(apenas inteiros)
+valores_impar = []
+soma_valores_impar = []
 
+entrada = input('Digite uma sequência de números inteiros, separados por espaço: ') # pegando dados
+for x in entrada: # percontendo pelos dados iputados
+    if x != ' ': # condição para pegar apenas os numeros
+        inteiros.append(int(x)) # colocando na lista interios, os inteiros de i
 
-while len(so_impar):
-    ...
+for y in inteiros:
+    if y % 2 != 0:
+        valores_impar.append(y)
+        
+while len(valores_impar) > 1:
+    soma_1 = valores_impar[0]
+    soma_2 = valores_impar[1]
+    soma_total = soma_1 + soma_2
+    soma_valores_impar.append(soma_total)
+    del valores_impar[0]
+print(soma_valores_impar)
